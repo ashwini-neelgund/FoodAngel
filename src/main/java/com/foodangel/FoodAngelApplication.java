0a409af04e9520dd5d6ae06f118078ea0d3283e4
Ashwini Neelgund <ashwinineelgundh@gmail.com>
2021-06-16 09:32:38 -0400

    Initial commit

---
+++ src/main/java/com/foodangel/FoodAngelApplication.java
@@ -0,0 +1,13 @@
+package com.foodangel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAngelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodangel/FoodAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodangel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAngelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
