d3b733c716e1f84dd9f663f2eaaa21921ed44c73
Ashwini <ashwinineelgundh@gmail.com>
2021-06-23 22:14:30 -0400

    removed unused import

--- src/main/java/com/foodangel/controller/SeekerController.java
+++ src/main/java/com/foodangel/controller/SeekerController.java
@@ -4,7 +4,6 @@ import com.foodangel.model.Item;
 import com.foodangel.model.Request;
 import com.foodangel.model.User;
 import com.foodangel.service.RequestService;
-import com.foodangel.service.UserService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,35 +24,35 @@ public class SeekerController {
     }
 
     @GetMapping("/request/{id}/{pin}")
-    public Request getRequest(@PathVariable("id") Long id,@PathVariable("pin") int pin){
-        return requestService.getRequest(id,pin);
+    public Request getRequest(@PathVariable("id") Long id, @PathVariable("pin") int pin) {
+        return requestService.getRequest(id, pin);
     }
 
     @PostMapping("/add/request")
-    public Request addRequest(@RequestBody User user){
+    public Request addRequest(@RequestBody User user) {
         return requestService.addRequest(user);
     }
 
     @PostMapping("/update/request")
-    public Request updateRequest(@RequestBody Request request){
+    public Request updateRequest(@RequestBody Request request) {
         return requestService.updateRequest(request);
     }
 
     @DeleteMapping("/remove/request/{requestId}")
-    public void deleteRequest(@PathVariable("requestId") Long requestId){
+    public void deleteRequest(@PathVariable("requestId") Long requestId) {
         requestService.removeRequest(requestId);
     }
 
     @GetMapping("/check/angel/{zipCode}")
-    public boolean checkForAngel(@PathVariable("zipCode") int zipcode){
-        if(requestService.checkForAngel(zipcode)){
+    public boolean checkForAngel(@PathVariable("zipCode") int zipcode) {
+        if (requestService.checkForAngel(zipcode)) {
             return true;
-        }else
+        } else
             return false;
     }
 
     @GetMapping("/items")
-    public List<Item> getItems(){
+    public List<Item> getItems() {
         return requestService.getItems();
     }
 }
